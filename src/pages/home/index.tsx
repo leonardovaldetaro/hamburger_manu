12c70be30cef311080ccc17bd0e102fe75b96a6f
leonardovaldetaro <leonardovaldetaro@gmail.com>
2024-01-23 21:24:43 +0000

    iniciando a home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,27 @@
 import Styles from './Home.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Home () {
     return (
-        <main className={Styles.main}>
-            <h1 className={Styles.main__title}>Olá Mundo!</h1>
-        </main>
+        <section className={Styles.main}>
+            <h2 className={Styles.main__name}>
+                Leonardo Valdetaro
+            </h2>
+            <h1 className={Styles.main__title}>
+                Front-end web developer,<br /> Digital designer.
+            </h1>
+            <p className={Styles.main__text}>
+                As a front-end web developer and digital designer, I'm passionate about creating professional, modern, and search engine-optimized websites that stand out on the internet and attract more customers.
+            </p>
+            <div className={Styles.main__botaoWraper}>
+                <Link className={Styles.main__botaoWraper__link} to={"/contact"}>
+                    Contact
+                </Link>
+                    or
+                <Link className={Styles.main__botaoWraper__link} to={"/about"}>
+                    Read About Me
+                </Link>
+            </div>
+        </section>
     );
 }
