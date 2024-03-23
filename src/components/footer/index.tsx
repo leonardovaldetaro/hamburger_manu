7958bf107fb3b66662d97d60340df564a2c6659f
leonardovaldetaro <leonardovaldetaro@gmail.com>
2024-03-23 11:22:09 +0000

    adicionando ilustracao do topo e concluindo o footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,78 @@
 import Styles from './Footer.module.scss';
+import classNames from 'classnames';
+import { ReactComponent as LogoNight } from 'assets/simbol_lv_branco.svg';
+import { ReactComponent as LogoDay } from 'assets/simbol_lv_preto.svg';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function Footer () {
+    const [isDay, setIsDay] = useState(true);
+
+    useEffect(()=> {
+        const horaAtual = new Date().getHours();
+        setIsDay(horaAtual >= 6 && horaAtual < 18);
+    }, []);
+    
     return (
-        <footer className={Styles.footer}>
+        <>
+        <footer className={classNames(
+            Styles.footer,
+            { [Styles.day]: isDay },
+            { [Styles.night]: !isDay }
+        )}>
             <div className={Styles.footer__container}>
-                <h5 className={Styles.footer__title}>Leonardo Valdetaro</h5>
-                <p className={Styles.footer__copyright}>&copy; Copyright 2024</p>
+                <h4 className={Styles.footer__container__titleContact}>
+                    Contact
+                    information —
+                </h4>
+                <p className={Styles.footer__container__textContact}>
+                    Feel free to reach out to me any time. I prefer to talk over email, especially since we may be a few time zones away.
+                </p>
+
+                <h4 className={Styles.footer__container__titleProjects}>
+                    Latest
+                    projects —
+                </h4>
+
+                <ul className={Styles.footer__container__list}>
+                    <li className={Styles.footer__container__list__item}>
+                        <Link to="/" className={Styles.footer__container__link}>Front-end</Link>
+                     </li>
+                    <li className={Styles.footer__container__list__item}>
+                        <Link to="/" className={Styles.footer__container__link}>Basic UI</Link>
+                    </li>
+                    <li className={Styles.footer__container__list__item}>
+                        <Link to="/" className={Styles.footer__container__link}>VIP</Link>
+                    </li>
+                    <li className={Styles.footer__container__list__item}>
+                        <Link to="/" className={Styles.footer__container__link}>Basic UX</Link>
+                    </li>
+                </ul>
+                <h4 className={Styles.footer__container__titleAvailability}>
+                    Current
+                    availability —
+                </h4>
+                <p className={Styles.footer__container__textAvailability}>
+                I usually work on several projects but I’ll be happy to discuss new opportunities. Let’s get in touch!
+                </p>
+
             </div>
         </footer>
+
+        <div className={classNames(
+            Styles.copyright,
+            { [Styles.day]: isDay },
+            { [Styles.night]: !isDay }
+        )}>
+
+            <div className={Styles.copyright__logoContainer}>
+                {isDay ? <LogoDay className={Styles.copyright__logoContainer__logo}/> : <LogoNight className={Styles.copyright__logoContainer__logo} />}
+                <h5 className={Styles.copyright__logoContainer__title}>Leonardo Valdetaro</h5>
+            </div>
+
+            <p className={Styles.copyright__copyright}>&copy; Copyright 2024</p>
+
+        </div>
+        </>
     );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,30 +2,35 @@ import Slide from 'components/slide';
 import Styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
 import CallToWork from './callToWork';
+import { ReactComponent as ImageTop } from 'assets/mobile_wireframe.svg'
 
 export default function Home () {
     return (
         <>
         <section className={Styles.main}>
-            <h2 className={Styles.main__name}>
-                Leonardo Valdetaro
-            </h2>
-            <h1 className={Styles.main__title}>
-                Front-end web developer,<br /> Digital designer.
-            </h1>
-            <p className={Styles.main__text}>
-                As a front-end web developer and digital designer, I'm passionate about creating professional, modern, and search engine-optimized websites that stand out on the internet and attract more customers.
-            </p>
-            <div className={Styles.main__botaoWraper}>
-                <Link className={Styles.main__botaoWraper__link} to={"/contact"}>
-                    Contact
-                </Link>
-                    or
-                <Link className={Styles.main__botaoWraper__link} to={"/about"}>
-                    Read About Me
-                </Link>
-            </div>
+            <ImageTop className={Styles.main__img} />
+            <article className={Styles.main__infoContainer}>
+                <h2 className={Styles.main__infoContainer__name}>
+                    Leonardo Valdetaro
+                </h2>
+                <h1 className={Styles.main__infoContainer__title}>
+                    Front-end web developer, Digital designer.
+                </h1>
+                <p className={Styles.main__infoContainer__text}>
+                    As a front-end web developer and digital designer, I'm passionate about creating professional, modern, and search engine-optimized websites that stand out on the internet and attract more customers.
+                </p>
+                <div className={Styles.main__botaoWraper}>
+                    <Link className={Styles.main__botaoWraper__link} to={"/contact"}>
+                        Contact
+                    </Link>
+                        or
+                    <Link className={Styles.main__botaoWraper__link} to={"/about"}>
+                        Read About Me
+                    </Link>
+                </div>
+            </article>
         </section>
+
         <section className={Styles.slide}>
             <h3 className={Styles.slide__subtitle}>selected projects</h3>
             <h2 className={Styles.slide__title}>Case Studies</h2>
